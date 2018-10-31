1ef5a9805f901b3df9f3836c338ba0dff78b87a4
patrickaloia <40310408+patrickaloia@users.noreply.github.com>
2018-10-31 13:25:05 -0400

    Updated product.js with imageUrl column

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -22,7 +22,12 @@ const Product = db.define('product', {
   inventoryQuantity: {
     type: Sequelize.INTEGER,
     allowNull: false
-  }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://cdn.shopify.com/s/files/1/2382/0223/products/30873-1_73d9e0c4-6737-4d59-a425-35536ad6c131_200x200.jpg?v=1526529402',
+    allowNull: false
+   }
 })
 
 module.exports = Product
