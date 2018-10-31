84eba0afb5387c10e4785ffd86156d25e7e400d7
Patrick Aloia <patrickalexanderaloia@gmail.com>
2018-10-31 14:34:03 -0400

    fixed the tests

--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -15,12 +15,18 @@ describe('Product Routes', () => {
     const productName = 'Funky Gravy'
     const description = 'The James Brown of gravy'
     const category = 'lumpy'
+    const price = 2
+    const inventoryQuantity = 33
+    const image_URL = 'https://cdn.shopify.com/s/files/1/2382/0223/products/30873-1_73d9e0c4-6737-4d59-a425-35536ad6c131_200x200.jpg?v=1526529402'
 
     beforeEach(() => {
       return Product.create({
         name: productName,
         description,
-        category
+        category,
+        price,
+        inventoryQuantity,
+        image_URL
       })
     })
 
