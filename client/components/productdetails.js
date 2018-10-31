4801f88549a33872cf3d8c139af87af9747a79cb
Patrick Aloia <patrickalexanderaloia@gmail.com>
2018-10-31 13:36:05 -0400

    first change to components for image_Url

--- client/components/productdetails.js
+++ client/components/productdetails.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 export default function ProductDetails(props) {
   return (
-    <td key={props.product.id}>
+    <li key={props.product.id}>
       {props.product.name}
+      <img src={props.product.image_Url} />
       <ul>
         <li>{props.product.description}</li>
         <li>{props.product.category}</li>
       </ul>
-    </td>
+    </li>
   )
 }
