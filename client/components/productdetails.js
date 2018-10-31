e4da1a7e2a381957f785cacd6b885e1f063cc7be
Patrick Aloia <patrickalexanderaloia@gmail.com>
2018-10-31 13:56:46 -0400

    refactored seed files and models

--- client/components/productdetails.js
+++ client/components/productdetails.js
@@ -4,10 +4,11 @@ export default function ProductDetails(props) {
   return (
     <li key={props.product.id}>
       {props.product.name}
-      <img src={props.product.image_Url} />
+      <img src={props.product.image_URL} />
       <ul>
         <li>{props.product.description}</li>
         <li>{props.product.category}</li>
+        <li>{props.product.price}</li>
       </ul>
     </li>
   )
--- server/db/models/product.js
+++ server/db/models/product.js
@@ -16,18 +16,17 @@ const Product = db.define('product', {
     type: Sequelize.TEXT
   },
   price: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.DECIMAL,
     allowNull: false
   },
   inventoryQuantity: {
     type: Sequelize.INTEGER,
     allowNull: false
   },
-  image_Url: {
+  image_URL: {
     type: Sequelize.STRING,
     defaultValue: 'https://cdn.shopify.com/s/files/1/2382/0223/products/30873-1_73d9e0c4-6737-4d59-a425-35536ad6c131_200x200.jpg?v=1526529402',
-    allowNull: false
-   }
+  }
 })
 
 module.exports = Product
