a72f2b3697dad6ad989ebc747ca488f8fdebf43c
patrickaloia <40310408+patrickaloia@users.noreply.github.com>
2018-11-01 22:56:54 -0400

    Commented out code to avoid conflicts

--- client/components/orderhistory.js
+++ client/components/orderhistory.js
@@ -1,33 +1,33 @@
-import React, {Component} from 'react'
-import axios from 'axios'
+// import React, {Component} from 'react'
+// import axios from 'axios'
 
-export default class OrderHistory extends Component {
-  const
-  constructor(props) {
-    super(props)
-    this.state = []
-  }
+// export default class OrderHistory extends Component {
+//   const
+//   constructor(props) {
+//     super(props)
+//     this.state = []
+//   }
 
-  async componentDidMount() {
-    const orders = await axios.get('/api/users/orders/me')
-    this.setState(orders)
-  }
+//   async componentDidMount() {
+//     const orders = await axios.get('/api/users/orders/me')
+//     this.setState(orders)
+//   }
 
-  render() {
-    if (!campus) {
-      return (
-        <div>
-          <h1>ORDER HISTORY</h1>
-          <p>There is currently no order history</p>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <h1>ORDER HISTORY</h1>
-          <p>ORDER HISTORY HERE</p>
-        </div>
-      )
-    }
-  }
-}
+//   render() {
+//     if (!campus) {
+//       return (
+//         <div>
+//           <h1>ORDER HISTORY</h1>
+//           <p>There is currently no order history</p>
+//         </div>
+//       )
+//     } else {
+//       return (
+//         <div>
+//           <h1>ORDER HISTORY</h1>
+//           <p>ORDER HISTORY HERE</p>
+//         </div>
+//       )
+//     }
+//   }
+// }
