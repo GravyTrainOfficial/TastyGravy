0bfe5ad10b13ed365aba98512beeaf53f77e7671
ARKielley <eatingfoil@gmail.com>
2018-11-04 23:19:28 -0500

    removed bad test

--- server/api/line-items.spec.js
+++ server/api/line-items.spec.js
@@ -64,10 +64,10 @@ describe('LineItem routes', () => {
       return LineItem.create(itemNotInCart)
     })
 
-    it('GET /api/line-items should return a 200 response if an admin user is logged in', function(done){
-      authenticatedUser.get('/api/line-items')
-      .expect(200);
-    });
+    // it('GET /api/line-items should return a 200 response if an admin user is logged in', function(done){
+    //   authenticatedUser.get('/api/line-items')
+    //   .expect(200);
+    // });
 
     it('GET /api/line-items responds successfully', async () => {
       const res = await authenticatedUser
