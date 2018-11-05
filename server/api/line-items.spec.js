8ddaa414f1378b8ba821c499146248f1c6847e29
ARKielley <eatingfoil@gmail.com>
2018-11-04 20:51:39 -0500

    attempted cart test

--- server/api/line-items.spec.js
+++ server/api/line-items.spec.js
@@ -13,8 +13,16 @@ describe('LineItem routes', () => {
   })
 
   describe('/api/line-items/', () => {
-    const quantity = 10
-    const productId = 1
+    const itemInCart = {
+      quantity: 10,
+      productId: 1,
+      status: 'cart',
+      userId
+    }
+    const itemNotInCart = {
+      quantity: 3,
+      productId: 2
+    }
     // const userCredentials = {
     //   email: 'heiscool@cool.com',
     //   password: 'test'
@@ -25,12 +33,12 @@ describe('LineItem routes', () => {
     }
     const authenticatedUser = request.agent(app)
 
-    before(function(done){
-      
-    });
-
     beforeEach((done) => {
       User.create(userCredentials)
+      const {id: {userId}} = User.findOne({ 
+        where: {email: userCredentials.email},
+        attributes: ['id']
+      })
       authenticatedUser
         .post('/login')
         .send(userCredentials)
@@ -39,10 +47,8 @@ describe('LineItem routes', () => {
           expect('Location', '/home');
           done();
         });
-      return LineItem.create({
-        quantity,
-        productId
-      })
+      LineItem.create(itemInCart)
+      return LineItem.create(itemNotInCart)
     })
 
     it('GET /api/line-items responds succcessfully', async () => {
@@ -51,8 +57,23 @@ describe('LineItem routes', () => {
         .expect(200)
         
       expect(res.body).to.be.an('array')
-      expect(res.body[0].quantity).to.be.equal(quantity)
-      expect(res.body[0].status).to.be.equal(status)
+      expect(res.body[1].quantity).to.be.equal(itemNotInCart.quantity)
+      expect(res.body[1].status).to.be.equal(itemNotInCart.status)
+    })
+
+    it('GET /api.line-items/cart responds with user cart', async () => {
+      const {id: {userId}} = User.findOne({ 
+        where: {email: userCredentials.email},
+        attributes: ['id']
+      })
+      const res = await request(app)
+        .get('/api/line-items/cart')
+        .expect(200)
+
+        expect (res.body).to.be.an('array')
+        expect (res.body[0].quantity).to.be.equal(itemInCart.quantity)
+        expect (res.body[0].status).to.be.equal(itemInCart.status)
+        expect (res.body[0].userId).to.be.equal(userId)
     })
 
     it('POST /api/line-items creates and responds successfully')
