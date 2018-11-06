69057ff13455aa716fce9623843380257a0a4c8d
ARKielley <eatingfoil@gmail.com>
2018-11-06 16:12:43 -0500

    added test for changeInventory

--- server/db/models/product.spec.js
+++ server/db/models/product.spec.js
@@ -2,4 +2,29 @@ const { expect } = require('chai')
 const db = require('../index')
 const Product = db.model('product')
 
-//tests go here
+describe('Product model', () => {
+  beforeEach(() => {
+    return db.sync({ force: true })
+  })
+
+  describe('instanceMethods', () => {
+    describe('changeInventory', () => {
+      let carrot
+
+      beforeEach(async () => {
+        carrot = await Product.create({
+          name: 'Carrot',
+          price: 100000,
+          inventoryQuantity: 10
+        })
+      })
+
+      it('changes the inventory by the amount specified', () => {
+        carrot.changeInventory(5)
+        expect(carrot.inventoryQuantity).to.be.equal(15)
+        carrot.changeInventory(-8)
+        expect(carrot.inventoryQuantity).to.be.equal(7)
+      })
+    }) // end describe('changeInventory')
+  }) // end describe('instanceMethods')
+}) // end describe('User model')
