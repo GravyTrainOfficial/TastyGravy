4865bb04cfe4a10ac09a03f56a18a70eb9d9a541
ARKielley <eatingfoil@gmail.com>
2018-11-06 09:23:25 -0500

    oops

--- client/components/item-preview.js
+++ client/components/item-preview.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { formatPrice } from '../util'
-import { modifyLineItem } from '../store/cart';
 import { connect } from 'react-redux';
 // withRouter
 
