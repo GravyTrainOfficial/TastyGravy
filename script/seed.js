e7b18c063b1943b8791227c8c505db2227176414
Patrick Aloia <patrickalexanderaloia@gmail.com>
2018-11-06 11:04:03 -0500

    first commit for seed file updates

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const db = require('../server/db')
-const {User, Product, LineItem, Order} = require('../server/db/models')
+const { User, Product, LineItem, Order } = require('../server/db/models')
 const devPassword = 'test'
 
 const users = [
@@ -44,88 +44,92 @@ const users = [
 
 const products = [
   {
-    name: 'Miconazole 3 Combination Pack',
+    name: "Moens Baert Truffle Gravy",
     description:
-      'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.',
-    category: 'Kids',
-    price: 1.5,
-    inventoryQuantity: 12,
+      "A Belgian delicacy, Moens Baert's Truffle Gravy has a smooth, rich flavor familiar to any serious purveyor of gravy.\n\nBest paired with game meats.",
+    category: 'Craft',
+    price: 7900,
+    inventoryQuantity: 10,
     image_URL: 'http://dummyimage.com/200x200.bmp/ff4444/ffffff'
   },
   {
-    name: 'Ludens Wild Honey Throat Drops',
+    name: 'Appalachian Honey Ham Gravy',
     description:
-      'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.',
-    category: 'Books',
-    price: 1.5,
-    inventoryQuantity: 12,
+      "Much like the mountains it hails from, the Appalachian Honey Ham has awed many a gravy-snob for generations. If you're having smoked ham without this bad boy,\n\nwhat are you even doing my man?",
+    category: 'Sweet',
+    price: 1400,
+    inventoryQuantity: 100,
     image_URL: 'http://dummyimage.com/200x200.png/5fa2dd/ffffff'
   },
   {
-    name: 'Linden',
+    name: 'Cow Shin Bone-Broth Gravy',
     description:
-      'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
-    category: 'Computers',
-    price: 1.5,
-    inventoryQuantity: 12,
+      "A staple in the North Shore of Long Island, Cow Shin drips the meat juices that make gravy directly from a cow's shin.\n\nDid you know people can make gravy from bones? I sure didn't, and it make me anxious!",
+    category: 'Bone Broth',
+    price: 5000,
+    inventoryQuantity: 15,
     image_URL: 'http://dummyimage.com/200x200.bmp/cc0000/ffffff'
   },
   {
-    name: 'SUPER AQUA MAX WHITE C LUMINOUS AMPOULE',
+    name: 'Sam Adams Traditional Thanksgiving Special',
     description:
-      'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.\n\nIn hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.',
-    category: 'Industrial',
-    price: 1.5,
-    inventoryQuantity: 12,
+      "People don't know this, but Sam Adams has been making gravy for longer than they've been making beer. They just aren't very good at it.",
+    category: 'American',
+    price: 150,
+    inventoryQuantity: 200,
     image_URL: 'http://dummyimage.com/200x200.jpg/ff4444/ffffff'
   },
   {
-    name: 'RUMEX ACETOSELLA POLLEN',
+    name: "Tasty Gravy's Twig & Gizzard Blend",
     description:
-      'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.\n\nAenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.',
-    category: 'Kids',
-    price: 1.5,
-    inventoryQuantity: 12
+      'A classic blend from our gravy-experts, always pushing the limits of what is "edible" and what "definitely is not edible".\n\nBest paired fowl or reptile.',
+    category: 'Giblet',
+    price: 2400,
+    inventoryQuantity: 110,
+    image_URL: 'http://dummyimage.com/200x200.jpg/ff4444/ffffff'
   },
   {
     name: 'Magnesium Chloride',
     description:
-      'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.',
-    category: 'Movies',
-    price: 1.5,
-    inventoryQuantity: 12
+      "Not sure how this got into the warehouse, but we have a lot of it and it'd be great if you could take it off our hands.\n\nPlease do not pair this with anything.",
+    category: 'Miscellaneous',
+    price: 1000,
+    inventoryQuantity: 500,
+    image_URL: 'http://dummyimage.com/200x200.jpg/ff4444/ffffff'
   },
   {
-    name: 'Metabolism',
+    name: "Tasty Gravy's Turkey Gravy",
     description:
-      'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.',
-    category: 'Computers',
-    price: 1.5,
-    inventoryQuantity: 12,
+      'The gravy that put us on the map. Nothing can match the thick, creamy flavors of our Turkey Gravy.\n\nBest paired with turkey, but pairs well with most meats.',
+    category: 'Brown',
+    price: 2500,
+    inventoryQuantity: 500,
     image_URL: 'http://dummyimage.com/200x200.jpg/cc0000/ffffff'
   },
   {
-    name: 'SHISEIDO RADIANT LIFTING FOUNDATION',
+    name: 'Shishito Pepper Truffle Gravy',
     description:
-      'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.',
-    category: 'Computers',
-    price: 1.5,
-    inventoryQuantity: 12
+      'Terrific gravy blend from our neighbors down south (Florida). As exquisite and fullbodied as it is cutting.',
+    category: 'Truffle',
+    price: 3500,
+    inventoryQuantity: 12,
+    image_URL: 'http://dummyimage.com/200x200.jpg/ff4444/ffffff'
   },
   {
-    name: 'HEADACHE',
+    name: "Gérard Dupont's White Roux",
     description:
-      'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.',
-    category: 'Industrial',
-    price: 1.5,
-    inventoryQuantity: 12
+      "As fantastic and bewildering in flavor as its originator, Gérard Dupont, infamous gravy theorist known for his harsh temper and flamboyant outfits.\n\nBest paired with veal, but works equally well with most vegetables.",
+    category: 'Roux',
+    price: 4000,
+    inventoryQuantity: 30,
+    image_URL: 'http://dummyimage.com/200x200.jpg/ff4444/ffffff'
   },
   {
     name: 'IOPE RETIGEN MOISTURE TWIN CAKE NO.23',
     description:
       'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.',
     category: 'Electronics',
-    price: 1.5,
+    price: 150,
     inventoryQuantity: 12,
     image_URL: 'http://dummyimage.com/200x200.png/cc0000/ffffff'
   },
@@ -134,116 +138,116 @@ const products = [
     description:
       'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.',
     category: 'Electronics',
-    price: 1.5,
+    price: 150,
     inventoryQuantity: 12,
     image_URL: 'http://dummyimage.com/200x200.jpg/ff4444/ffffff'
   },
   {
-    name: 'Spironolactone',
+    name: 'A big asssss',
     description:
-      'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.',
-    category: 'Jewelery',
-    price: 1.5,
-    inventoryQuantity: 12,
+      'Just a,\n\n  a real nasty butttt ',
+    category: 'Sweet',
+    price: 999900,
+    inventoryQuantity: 9999,
     image_URL: 'http://dummyimage.com/200x200.bmp/dddddd/000000'
   },
   {
-    name: 'STUDIO FIX FLUID SPF 15 BROAD SPECTRUM',
+    name: "Nutella Gravy",
     description:
-      'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.',
-    category: 'Toys',
-    price: 1.5,
-    inventoryQuantity: 12,
-    image_URL: 'http://dummyimage.com/200x200.png/cc0000/ffffff'
+      "An original Tasty Gravy blend, where we take everyone's favorite sweet spread and make it\n\ninto a slightly less viscous version of the same treat. Best paired with baked goods.",
+    category: 'Sweet',
+    price: 1800,
+    inventoryQuantity: 75,
+    image_URL: 'http://dummyimage.com/200x200.bmp/ff4444/ffffff'
   },
   {
     name: 'ANTIMICROBIAL FOAMING',
     description:
       'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.\n\nDuis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
     category: 'Health',
-    price: 1.5,
+    price: 150,
     inventoryQuantity: 12,
     image_URL: 'http://dummyimage.com/200x200.bmp/ff4444/ffffff'
   },
   {
-    name: 'Rhodotorula mucilaginosa',
+    name: 'Rhinocerous Horn Brew',
     description:
-      'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.',
-    category: 'Games',
-    price: 1.5,
-    inventoryQuantity: 12,
+      'It was not illegal at the time we first made this. It was definitely unethical though.',
+    category: 'Bone',
+    price: 12000,
+    inventoryQuantity: 3,
     image_URL: 'http://dummyimage.com/200x200.png/ff4444/ffffff'
   },
   {
-    name: 'Herbion Naturals Orange',
+    name: 'Norton Falls Citrus Brew',
     description:
-      'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
-    category: 'Computers',
-    price: 1.5,
-    inventoryQuantity: 12,
+      'A fascinating take on the citrus micro-brew craze sweeping the Gravy Nation. Our tasters were floored.\n\nBest paired with beef, but pairs well with pork.',
+    category: 'Vegetarian',
+    price: 2000,
+    inventoryQuantity: 99,
     image_URL: 'http://dummyimage.com/200x200.png/cc0000/ffffff'
   },
   {
-    name: 'Dog Hair',
+    name: "R'lyeh Blend",
     description:
-      'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.',
-    category: 'Baby',
-    price: 1.5,
-    inventoryQuantity: 12,
-    image_URL: 'http://dummyimage.com/200x200.bmp/ff4444/ffffff'
-  },
-  {
-    name: 'AFLURIA',
-    description:
-      'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.',
-    category: 'Books',
-    price: 1.5,
+      "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn!",
+    category: 'Eldritch Horror',
+    price: 10000,
     inventoryQuantity: 12,
     image_URL: 'http://dummyimage.com/200x200.jpg/dddddd/000000'
   },
   {
-    name: 'FAMILY DOLLAR ACNE TREATMENT',
+    name: 'Chicken Stock Blend',
     description:
-      'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.',
-    category: 'Automotive',
-    price: 1.5,
-    inventoryQuantity: 12,
+      'For suppliers and restaurants, 10 barrels of our finest chicken gravy stock.',
+    category: 'Supplier',
+    price: 2000,
+    inventoryQuantity: 100,
+    image_URL: 'http://dummyimage.com/200x200.jpg/dddddd/000000'
+  },
+  {
+    name: 'Cow Stock Blend',
+    description:
+      'For suppliers and restaurants, 10 barrels of our finest cow gravy stock.',
+    category: 'Supplier',
+    price: 2500,
+    inventoryQuantity: 75,
     image_URL: 'http://dummyimage.com/200x200.jpg/5fa2dd/ffffff'
   },
   {
-    name: 'Naratriptan',
+    name: "Dr. Robert's Magic Gravy",
     description:
-      'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.',
-    category: 'Movies',
-    price: 1.5,
-    inventoryQuantity: 12,
-    image_URL: 'http://dummyimage.com/200x200.jpg/cc0000/ffffff'
+      "Dr. Robert has been making gravy for over 400 years. His latest brew will knock your socks off -- and more!",
+    category: 'Magic Gravy',
+    price: 100000,
+    inventoryQuantity: 1,
+    image_URL: 'http://dummyimage.com/200x200.bmp/ff4444/ffffff'
   },
   {
-    name: 'FACE IT RADIANCE POWDER PACT SPF25 MOISTURE VEIL NB21',
+    name: 'Graceland Turkey Blend',
     description:
-      'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.',
-    category: 'Toys',
-    price: 1.5,
-    inventoryQuantity: 12,
+      "This Graceland treat will have you dancing in your blue suede shoes and saying 'Thank you, thank you very much...for the gravy!'\n\nBest paired with southern fried foods.",
+    category: 'Brown',
+    price: 2200,
+    inventoryQuantity: 80,
     image_URL: 'http://dummyimage.com/200x200.jpg/dddddd/000000'
   },
   {
-    name: 'Laxative Pills',
+    name: 'Redeye Blood Gravy',
     description:
-      'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.',
-    category: 'Baby',
-    price: 1.5,
+      'Bloody delicious, you can taste the iron!\n\nBest paired with a juicy steak.',
+    category: 'Redeye',
+    price: 3300,
     inventoryQuantity: 12,
     image_URL: 'http://dummyimage.com/200x200.png/ff4444/ffffff'
   },
   {
-    name: 'Intuniv',
+    name: 'Salsiccia Siciliano',
     description:
-      'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.',
-    category: 'Kids',
-    price: 1.5,
-    inventoryQuantity: 12,
+      'Molto bene! A sausage-based gravy from Sicilio. Mamma mia! Etc.!\n\nBest paired with a pizza pasta gabagool!',
+    category: 'Sausage',
+    price: 5900,
+    inventoryQuantity: 4000,
     image_URL: 'http://dummyimage.com/200x200.bmp/cc0000/ffffff'
   },
   {
@@ -251,7 +255,7 @@ const products = [
     description:
       'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.',
     category: 'Automotive',
-    price: 1.5,
+    price: 150,
     inventoryQuantity: 12,
     image_URL: 'http://dummyimage.com/200x200.png/dddddd/000000'
   },
@@ -260,7 +264,7 @@ const products = [
     description:
       'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.',
     category: 'Sports',
-    price: 1.5,
+    price: 150,
     inventoryQuantity: 12,
     image_URL: 'http://dummyimage.com/200x200.bmp/5fa2dd/ffffff'
   },
@@ -269,7 +273,7 @@ const products = [
     description:
       'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.\n\nNullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.',
     category: 'Baby',
-    price: 1.5,
+    price: 150,
     inventoryQuantity: 12,
     image_URL: 'http://dummyimage.com/200x200.png/5fa2dd/ffffff'
   },
@@ -278,7 +282,7 @@ const products = [
     description:
       'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.\n\nFusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.',
     category: 'Industrial',
-    price: 1.5,
+    price: 150,
     inventoryQuantity: 12,
     image_URL: 'http://dummyimage.com/200x200.jpg/cc0000/ffffff'
   },
@@ -287,7 +291,7 @@ const products = [
     description:
       'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.',
     category: 'Industrial',
-    price: 1.5,
+    price: 150,
     inventoryQuantity: 12,
     image_URL: 'http://dummyimage.com/200x200.bmp/ff4444/ffffff'
   },
@@ -296,7 +300,7 @@ const products = [
     description:
       'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
     category: 'Industrial',
-    price: 1.5,
+    price: 150,
     inventoryQuantity: 12,
     image_URL: 'http://dummyimage.com/200x200.png/dddddd/000000'
   },
@@ -305,7 +309,7 @@ const products = [
     description:
       'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.',
     category: 'Computers',
-    price: 1.5,
+    price: 150,
     inventoryQuantity: 12,
     image_URL: 'http://dummyimage.com/200x200.png/cc0000/ffffff'
   }
@@ -350,7 +354,7 @@ const lineItems = [
 ]
 
 async function seed() {
-  await db.sync({force: true})
+  await db.sync({ force: true })
   console.log('db synced!')
 
   await Promise.all(users.map(user => User.create(user)))
